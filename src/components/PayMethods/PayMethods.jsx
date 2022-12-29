ada16c78593a24c0bf1e8db82836e67d1887613f
Mateo Leiva <mateoleiva50@gmail.com>
2022-12-29 20:47:50 -0300

    fix desktop payment methods section

--- src/components/PayMethods/PayMethods.jsx
+++ src/components/PayMethods/PayMethods.jsx
@@ -4,16 +4,20 @@ import truck from '../../assets/truck.png';
 
 const PayMethods = () => {
 	return (
-		<div className='bg-[#FEFEFE] flex items-center shadow shadow-black/20'>
-			<div className='flex items-center justify-center flex-1'>
-				<h4>Payment Methods:</h4>
+		<div className='bg-[#FEFEFE] p-20 gap-32 flex-wrap flex items-center shadow shadow-black/20'>
+			<div className='flex items-center justify-end gap-8 flex-1'>
+				<h4 className='fontStylePlayfair text-[#007994] font-medium text-3xl'>
+					Payment Methods:
+				</h4>
 				<img src={zelle} alt='Zelle logo' />
 				<img src={cash} alt='CashApp logo' />
 			</div>
-			<span className='text-5xl font-thin m-auto text-[#D9D9D9]'>|</span>
-			<div className='flex items-center justify-center flex-1'>
+			<span className='p-0.5 h-20 bg-[#D9D9D9]'></span>
+			<div className='flex items-center gap-16 flex-1'>
 				<img src={truck} alt='' className='bg-[#FFF3C7] p-4 rounded-md' />
-				<p>Delivery in Tampa and surroundings.</p>
+				<p className='fontStylePlayfair font-medium text-3xl'>
+					Delivery in Tampa <br /> and surroundings.
+				</p>
 			</div>
 		</div>
 	);
