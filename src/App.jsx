9d5422117e39f21346f5fb7c34848a6784b28ca2
Mateo Leiva <mateoleiva50@gmail.com>
2023-01-06 19:37:26 -0300

    add navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Footer from './components/Footer/Footer';
+import Nav from './components/Nav/Nav';
 import PayMethods from './components/PayMethods/PayMethods';
 import Subscribe from './components/Subscribe/Subscribe';
 import Us from './components/Us/Us';
@@ -6,6 +7,7 @@ import Us from './components/Us/Us';
 function App() {
 	return (
 		<div className='App'>
+			<Nav />
 			<Us />
 			<PayMethods />
 			<Subscribe />
