11626f99d3e79d35c65e5b260665d03a2df66467
Mateo Leiva <mateoleiva50@gmail.com>
2023-01-09 20:20:44 -0300

    us section tablet

--- src/components/Us/Us.jsx
+++ src/components/Us/Us.jsx
@@ -3,15 +3,15 @@ import bg from '../../assets/bgus.png';
 
 const Us = () => {
 	return (
-		<div className='sm:h-screen flex flex-col sm:flex-row bg-[#FEFEFE]'>
+		<div className='lg:h-screen flex flex-col lg:flex-row bg-[#FEFEFE]'>
 			<div
 				style={{ backgroundImage: `url(${bg})` }}
-				className='bg-[#006C84] text-center px-9 pt-12 pb-28 sm:p-0 w-full sm:w-2/3 flex flex-col justify-center items-center'
+				className='bg-[#006C84] text-center px-9 pt-12 pb-28 md:px-16 md:pt-20 md:pb-36 lg:p-0 w-full lg:w-2/3 flex flex-col justify-center items-center'
 			>
-				<h2 className='text-[#FFE227] w-full fontStylePlayfair text-[40px] font-bold mb-6 sm:mb-11'>
+				<h2 className='text-[#FFE227] w-full fontStylePlayfair text-[40px] font-bold mb-6 lg:mb-11'>
 					ABOUT US
 				</h2>
-				<p className='sm:max-w-[50%] text-[13px] sm:text-[25px] text-white leading-6 sm:leading-[46px] text-justify fontStyleNeurial'>
+				<p className='lg:max-w-[60%] xl:max-w-[50%] text-[13px] sm:text-[18px] md:text-[22px] lg:text-[25px] text-white leading-6 sm:leading-7 md:leading-9 lg:leading-[46px] text-justify fontStyleNeurial'>
 					We are a store gifts and breakfasts specialized surprise in
 					personalizing each one of them. We are your accomplices to help you
 					create happy moments and recognize the people you value through our
@@ -20,11 +20,11 @@ const Us = () => {
 				</p>
 			</div>
 
-			<div className='px-[25%] pb-12 sm:p-0 sm:flex justify-center items-center sm:w-1/3'>
+			<div className='px-[25%] md:px-[30%] pb-12 lg:p-0 lg:flex justify-center items-center lg:w-1/3'>
 				<img
 					src={girl}
 					alt='birthday girl'
-					className='-mt-20 sm:mt-0 sm:mr-80'
+					className='-mt-20 lg:mt-0 lg:mr-48 xl:mr-72'
 				/>
 			</div>
 		</div>
